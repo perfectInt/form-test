1cfe8f1c14b146b9dbb4ca8dd12bc933095c51a9
TheShyen <ruslankaslesh@mail.ru>
2023-07-15 17:27:54 +0500

    basic functionality  form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -17,29 +17,34 @@
         />
 
         <q-select v-model="model" :options="options" class="col-md-6 text-h6" filled label="Category"/>
-        <q-uploader
-          bordered
-          class="col-md-6 text-h6"
-          color="purple"
-          flat
-          label="Upload files"
-          square
-
-          url="http://localhost:4444/upload"
-        />
+<!--        <q-uploader-->
+<!--          bordered-->
+<!--          class="col-md-6 text-h6"-->
+<!--          color="purple"-->
+<!--          flat-->
+<!--          label="Upload files"-->
+<!--          v-model="body"-->
+<!--          square-->
+<!--          url="http://localhost:3000/posts"-->
+<!--        />-->
+        <upload-file class="col-md-6"/>
         <div class="col-md-6 ">
           <q-btn color="primary" label="Submit" size="17px" type="submit"/>
           <q-btn class="q-ml-sm" color="primary" flat label="Reset" size="17px" type="reset"/>
         </div>
       </q-form>
+      <get-image/>
     </q-page-container>
 
   </q-layout>
 </template>
 <script setup>
 import {useQuasar} from "quasar";
-import {ref} from "vue";
+import { ref} from "vue";
 import axios from 'axios';
+import UploadFile from "./UploadFile.vue";
+import GetImage from "./GetImage.vue";
+
 
 const $q = useQuasar()
 
@@ -53,13 +58,12 @@ const options = [
 
 function onSubmit() {
 
-  
-  axios.post('https://jsonplaceholder.typicode.com/posts?_limit=10', JSON.stringify({
-    userId: Date.now(),
+
+  axios.post('http://localhost:3000/posts', {
     title: title.value,
     body: model.value,
-  }))
-    .then((response) => console.log(response))
+  })
+    .then((response) => console.log(response.data))
 
 
 }
---
+++ src/components/GetImage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <img :src="imageUrl" alt="Загруженное изображение" v-if="imageUrl" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      imageUrl: null,
+    };
+  },
+  mounted() {
+    this.fetchImage();
+  },
+  methods: {
+    fetchImage() {
+      axios
+        .get('https://dog.ceo/api/breeds/image/random')
+        .then(response => {
+          console.log(response)
+          this.imageUrl = response.data.message;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
